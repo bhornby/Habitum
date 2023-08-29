c07adc2e231869c29e53c85e0b945fa64d94fd4b
bhornby <70747776+bhornby@users.noreply.github.com>
2023-08-29 10:30:22 +0100

    finished real time habit updatinggit add .! im so proud of you benn:))))

--- launchpad.py
+++ launchpad.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 import json
 from title_screen import Userclass
 from title_screen import load_account
@@ -7,14 +8,28 @@ from title_screen import load_account
 def set_stage(i):
     st.session_state.stage = i
 
-def display_metrics(user):
-    habits = user.user_dictionary["habits"]
+def display_metrics():
+    habits = st.session_state.habits
+    
     # tells you the number of keys - therefore habits
     num_habits = len(habits) 
-    st.metric(label="Habits Completed", value=num_habits, delta=2.0,delta_color="normal")
+    habits_completed = 0
+    now = datetime.now()
+    today = now.strftime("%m/%d/%Y")
+    for key in habits:
+        if today in habits[key]["dates_done"]:
+            habits_completed += 1
+        # end if
+    # next key
+    habits_remaining = num_habits - habits_completed
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(label="Habits Completed", value=habits_completed, delta=habits_completed,delta_color="normal")
+    with col2:
+        st.metric(label="Habits Remaining", value=habits_remaining, delta=-habits_completed, delta_color="inverse")
 
-def display_habits(user):
-    habits = user.user_dictionary["habits"]
+def display_habits():
+    habits = st.session_state.habits
     # use a checkbox column
     fit_flag = 0
     fin_flag = 0
@@ -32,14 +47,15 @@ def display_habits(user):
     
     if fit_flag > 0:
         make_checkbox_col(habits, "fit")
-    if fin_flag > 0:
-        make_checkbox_col(habits, "fin")
-    if rel_flag > 0:
-        make_checkbox_col(habits, "rel")
-    if acc_flag > 0:
-        make_checkbox_col(habits, "acc")
+    # if fin_flag > 0:
+    #     make_checkbox_col(habits, "fin")
+    # if rel_flag > 0:
+    #     make_checkbox_col(habits, "rel")
+    # if acc_flag > 0:
+    #     make_checkbox_col(habits, "acc")
 
 def make_checkbox_col(habits, type):
+    count = 0
     habit_name = f"{type}_hab"
     widget_list = []
     fav_list = []
@@ -48,8 +64,10 @@ def make_checkbox_col(habits, type):
             widget_list.append(habits[key]["desc"])
         # end if
 
-    for hab in widget_list:
-        fav_list.append(False)
+    if count == 0:
+        for hab in widget_list:
+            fav_list.append(False)
+        count += 1
     
     if habit_name == "fit_hab":
         habit_name = "Fitness"
@@ -59,27 +77,60 @@ def make_checkbox_col(habits, type):
         habit_name = "Relationships"
     elif habit_name == "acc_hab":
         habit_name = "Accademic and Work"
-    
+        
     data_df = pd.DataFrame(
         {
             habit_name:widget_list,
             "favourite":fav_list,
         }
-    )
+    )   
 
     st.data_editor(
         data_df,
         column_config={
             "favourite": st.column_config.CheckboxColumn(
-                "Completed?",
+                "Completed",
                 help="Check your habit when **completed**",
-                width="large"
+                width="large",
+                required=True,
             )
         },
         disabled=["Habits"],
         hide_index=True,
-    )    
-# end function
+        key=habit_name,
+    )  
+    
+    if "Fitness" in st.session_state:
+        fit_dict = st.session_state.Fitness
+        fit_true_pos = []
+        box_num = 0
+        for checkbox in fit_dict["edited_rows"]:
+            for favourite in fit_dict["edited_rows"][checkbox]:
+                chosen = fit_dict["edited_rows"][checkbox][favourite]
+                st.write(box_num)
+                # if the box is checked - add its position to the fit true pos
+                if chosen == True:
+                    fit_true_pos.append(box_num)
+                # end if
+                box_num += 1
+            # next favourite
+        # next checkbox
+
+        # now to find which habit it corresponds to and set the date done
+        for pos in fit_true_pos:
+            hab = widget_list[pos]
+            now = datetime.now()
+            today = now.strftime("%m/%d/%Y")
+            for key in habits:
+                if habits[key]["desc"] == hab:
+                    habits[key]["dates_done"] = today
+                # end if
+            # next key
+        # next pos
+        st.session_state.habits = habits
+    # end if
+                
+# end function    
 
 def launchpad(username, password):
     # --- HOME PAGE ---
@@ -89,10 +140,15 @@ def launchpad(username, password):
         username = (st.session_state.username) 
         username = username.capitalize()
         st.title(f"{username}'s Launchpad")
+
+        if "habits" not in st.session_state:
+            st.session_state.habits = user.user_dictionary["habits"]
     
-        display_metrics(user)
+        display_metrics()
         st.divider()
-        display_habits(user)
+        display_habits()
+
+        st.write(st.session_state)
 # end function
 
 
