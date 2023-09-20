880fa731073d7e34c0ce73642f3fadcddc92df06
bhornby <70747776+bhornby@users.noreply.github.com>
2023-09-20 10:55:15 +0100

    good progress on the test file, the next step is to get access t the data stored in the files, currently we only have access to the name of the files

--- digital_ocean_file_test.py
+++ digital_ocean_file_test.py
@@ -1,8 +1,11 @@
 from boto3 import session
 from botocore.client import Config
+import json
 
 ACCESS_ID = 'DO00YD8PQZ9D7JNEZ97J'
 SECRET_KEY = 'uFyI3fQ2KEJbd4ndU/LoNmlI/xcUZyyvs9RQ2qnd0hg'
+user = "test"
+file = None
 
 # Initiate session
 session = session.Session()
@@ -12,5 +15,31 @@ client = session.client('s3',
                         aws_access_key_id=ACCESS_ID,
                         aws_secret_access_key=SECRET_KEY)
 
+# only want to upload the file when the user first creates their account,
+# after that all do is read and write to the file
+
+def upload():
 # Upload a file to your Space
-client.upload_file('beninformation.json', 'habitum-user-data', 'user_data/ben_information.json')
+    client.upload_file('test.json', 'habitum-user-data', 'user_data/test.json')
+# end if
+
+def bucket_list():
+    # returns a list of all the files in the bucket
+    response = client.list_objects(Bucket='habitum-user-data')
+    for obj in response['Contents']:
+        print(obj['Key'])
+# end function   
+
+# return the user information so we can manipulate it
+def locate_user(user):
+    folder_list = []
+    response = client.list_objects(Bucket="habitum-user-data")
+    for folder in response['Contents']:
+        name = folder["Key"]
+        folder_list.append(name)
+    # next folder
+    print(folder_list)
+
+# Thank you can check against all the use of data stored in the bucket and once you take that you check against username and password that creates that file name
+locate_user(user)
+# upload()
