3b335068583eae7bb28851c418f2245d1493ab21
bhornby <70747776+bhornby@users.noreply.github.com>
2023-10-06 11:47:04 +0100

    so proud that was intesnse so many small bugs were fixed, and the final big one was solved in the last minute so proud. What happended was there was an issue with the filename and passing the string through, it was a logic issue but once the logic was sound it was perf, well done ben

--- digital_ocean_file_test.py
+++ digital_ocean_file_test.py
@@ -1,6 +1,8 @@
 from boto3 import session
+import boto3
 from botocore.client import Config
 import json
+# import streamlit as st
 
 ACCESS_ID = 'DO00YD8PQZ9D7JNEZ97J'
 SECRET_KEY = 'uFyI3fQ2KEJbd4ndU/LoNmlI/xcUZyyvs9RQ2qnd0hg'
@@ -17,54 +19,65 @@ client = session.client('s3',
 # only want to upload the file when the user first creates their account,
 # after that all do is read and write to the file
 
-def upload():
+def upload(filename):
 # Upload a file to your Space
-    client.upload_file('test.json', 'habitum-user-data', 'user_data/test.json')
+# idealy should wipe the existing file --- 
+    s3 = boto3.resource('s3')
+    s3.Bucket('habitum-user-data').upload_file(Filename = filename, Key = filename)
+
 # end if
 
-def bucket_list():
-    # returns a list of all the files in the bucket
-    response = client.list_objects(Bucket='habitum-user-data')
-    for obj in response['Contents']:
-        print(obj['Key'])
-# end function   
-
-# return the user information so we can manipulate it
-def locate_user():
-    folder_list = []
-    user_data = None
-    response = client.list_objects(Bucket="habitum-user-data")
-    for folder in response['Contents']:
-        name = folder["Key"]
-        folder_list.append(name)
-    # next folder
-# end function
-
-def test_download():
+def test_download(filename):
+    
     user_dict = None
+    path = f'/Users/ben/projects/habit-tracker/{filename}'
+    path = str(path)
+   
     
     # downloads and replaces old data with new data
-    client.download_file(Bucket="habitum-user-data", Key="test.json", Filename="/Users/ben/projects/habit-tracker/test.json" )
-
-    to_change = input("what is your type?")
+    client.download_file('habitum-user-data', filename, path)
+    
+    # client.download_file(Bucket="habitum-user-data", Key="test.json", Filename="/Users/ben/projects/habit-tracker/test.json" )
 
     # goes into the json file and opens it
-    with open("test.json", "r", encoding="utf-8") as json_file:
-        user_dict =  json.load(json_file)
+    with open(filename, "r", encoding="utf-8") as json_file:
+        user_dict = json.load(json_file)
+        print(user_dict)
 
+    to_change = input("what is your type?")
     user_dict["type"] = to_change
 
     # update the local json file
-    with open("test.json", "w", encoding="utf-8") as json_file:
+    with open(filename, "w", encoding="utf-8") as json_file:
         json.dump(user_dict, json_file)
 
     # then need to update the digital ocean information - upload into an existing file - should replace the old with the new
-    client.put_object(Bucket='habitum-user-data',
-                  Key='test.json',
-                  Body="test.json",
-                  ACL='private',
-                )
+    upload(filename)
+# end function
 
-test_download()
+def demo():
+    flag = None
+    username = input("What is your username: ")
+    filename = None
+    if username == "":
+        flag = False
+    else:
+        flag = True
+    # end if
+    
+    if flag == True:
+        filename = f"{username}.json"
+         # now type string instead of nonetype
+        # filename = str(filename)
+        test_download(filename)
+    # end if    
+# end function
 
+demo()
+
+# if you want to use steamlit need so store the information in the session state
+# Invalid type for parameter Key, value: None, type: <class 'NoneType'>, valid types: <class 'str'>
+
+    
+    
 
